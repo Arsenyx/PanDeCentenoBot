a5aae4031f7f2c656bdc07a083bb05135b48f795
Arsenyx <ubootmodell@yahoo.com>
2025-05-01 15:51:29 +0200

    Updates files 3 main.py & start.py. Version 1.01

--- handlers/start.py
+++ handlers/start.py
@@ -1,6 +1,11 @@
 from telegram import ReplyKeyboardMarkup
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 async def start(update, context):
+    logging.info("/start command called")
+
     # Создаем клавиатуру с кнопками
     keyboard = [["Сделать заказ", "Меню", "Помощь"]]
     reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
