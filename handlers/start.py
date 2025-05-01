239deefdfb8e42b325a4b19cce7992ea982edda7
Arsenyx <ubootmodell@yahoo.com>
2025-05-01 12:56:21 +0200

    Delete icon pizza. Версия 1.01

--- handlers/start.py
+++ handlers/start.py
@@ -3,5 +3,5 @@ from telegram.ext import ContextTypes
 from keyboards.main_keyboard import main_keyboard
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Добро пожаловать в хлебную лавку 🍞! 🤯", reply_markup=main_keyboard)
+    await update.message.reply_text("¡ Добро пожаловать в хлебную лавку 🍞! ", reply_markup=main_keyboard)
     return 0  # MAIN_MENU
